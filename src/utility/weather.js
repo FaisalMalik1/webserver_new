37cc452195e593f004b5b8f06b425b9796a253b5
FaisalMalik1 <mominfaisalmalik26@gmail.com>
2022-03-30 16:02:03 +0530

    initial commit

---
+++ src/utility/weather.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const get_weather = (city, callback) => {
+    const weather_key = '075c50d3ffaba4d626185d22fe8b821e'
+    const url = 'http://api.weatherstack.com/current?access_key=' + weather_key + '&query=' + city + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect with Server!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            console.log("in api()", body)
+            callback(undefined, body)
+        }
+    })
+}
+
+module.exports = { get_weather: get_weather }
