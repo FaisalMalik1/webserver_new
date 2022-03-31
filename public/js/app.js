e98994cfc1f6dec81c79c70d402e56040f9f9a59
FaisalMalik1 <mominfaisalmalik26@gmail.com>
2022-03-31 10:55:21 +0530

    submission commit

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,9 @@ const search = document.querySelector('input')
 const cityname = document.querySelector('#cityname')
 const country = document.querySelector('#country')
 const temp = document.querySelector('#temp')
-
+const img = document.querySelector("#img")
+const description = document.querySelector("#description")
+let path
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -22,8 +24,11 @@ weatherForm.addEventListener('submit', (e) => {
                 cityname.textContent = data.error
             } else {
                 console.log(data)
+                path = data.current.weather_icons[0]
+                img.setAttribute('src', path)
                 cityname.textContent = data.location.name
                 country.textContent = data.location.country
+                description.textContent = data.current.weather_description[0]
                 temp.textContent = data.current.temperature + "°F"
             }
         })
